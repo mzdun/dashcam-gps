41853a1d861a6d92e6fe89397a0bdb4775414a6d
Marcin Zdun <mzdun@midnightbits.com>
2020-01-15 20:25:06 +0100

    [Android] Logging more like Chromium
    Certainly with potential to use __builtin_FILE, once it is available.

--- extras/android/log.hh
+++ extras/android/log.hh
@@ -1,4 +1,5 @@
 #include <android/log.h>
+#include <sstream>
 
 #define MAKE_LOG(string)                                              \
 	\
@@ -6,34 +7,85 @@ namespace {                                                           \
 		struct LOG_TAG__ {                                            \
 			static char const* tag_name() noexcept { return string; } \
 		};                                                            \
-		android::logger<LOG_TAG__> const Log{};                       \
+		using Logger = android::logger<LOG_TAG__>;                    \
 	\
 }
 
-namespace android {
-	template <class TagName>
-	struct logger {
-		template <int PRIORITY>
-		struct messages {
-			int operator()(const char* fmt, ...) const noexcept {
-				va_list ap;
-				va_start(ap, fmt);
-				auto const result = __android_log_vprint(
-				    PRIORITY, TagName::tag_name(), fmt, ap);
-				va_end(ap);
-				return result;
-			}
-			int operator()(std::error_code const& ec) const noexcept {
-				return (*this)("%s %d: %s", ec.category().name(), ec.value(),
-				               ec.message().c_str());
-			}
-		};
+#if __has_builtin(__builtin_FILE)
+#define LOG(LVL) \
+	Logger { ANDROID_LOG_##LVL }
+#else
+#define LOG(LVL) \
+	Logger { ANDROID_LOG_##LVL, __FILE__, __LINE__ }
+#endif
 
-		messages<ANDROID_LOG_VERBOSE> v{};
-		messages<ANDROID_LOG_DEBUG> d{};
-		messages<ANDROID_LOG_INFO> i{};
-		messages<ANDROID_LOG_WARN> w{};
-		messages<ANDROID_LOG_ERROR> e{};
-		messages<ANDROID_LOG_FATAL> f{};
+namespace android {
+#if __has_builtin(__builtin_FILE)
+	struct source_location {
+		static constexpr source_location current(
+		    const char* file = __builtin_FILE(),
+		    int line = __builtin_LINE()) noexcept {
+			source_location loc;
+			loc.file_ = file;
+			loc.line_ = line;
+			return loc;
+		}
+		static constexpr source_location current() noexcept { return {}; }
+
+		constexpr source_location() noexcept
+		    : file_("unknown"), func_(file), line_(0) {}
+
+		// 14.1.3, source_location field access
+		constexpr uint_least32_t line() const noexcept { return line_; }
+		constexpr const char* function_name() const noexcept { return func_; }
+
+	private:
+		const char* file_;
+		uint_least32_t line_;
+	};
+#endif  // has __builtin_FILE
+
+	template <class TagName>
+	class logger {
+	public:
+#if __has_builtin(__builtin_FILE)
+		logger(int priority,
+		       source_location const location = source_location::current())
+		    : priority_{priority}, location_{location} {}
+#else
+		logger(int priority, const char* file, uint_least32_t line)
+		    : priority_{priority}, file_{file}, line_{line} {}
+#endif
+
+		~logger() {
+			if (dirty_) {
+				__android_log_print(priority_, TagName::tag_name(),
+				                    "[%s:%u] > %s",
+#if __has_builtin(__builtin_FILE)
+				                    location_.file_name(), location_.line(),
+#else
+				                    file_, line_,
+#endif
+				                    o_.str().c_str());
+			}
+		}
+
+		template <typename Arg>
+		logger& operator<<(Arg&& arg) {
+			o_ << std::forward<Arg>(arg);
+			dirty_ = true;
+			return *this;
+		}
+
+	private:
+		std::ostringstream o_{};
+		bool dirty_{false};
+		int const priority_;
+#if __has_builtin(__builtin_FILE)
+		source_location const location_;
+#else
+		const char* file_;
+		uint_least32_t line_;
+#endif
 	};
 }
